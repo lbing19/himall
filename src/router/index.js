8b73f2a94369b5c2d4139cff42afdc6dfc7e2752
lbing19 <68732146+lbing19@users.noreply.github.com>
2020-08-18 20:13:11 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,41 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+//解决路由3.0以上报错Avoided redundant navigation to current location:
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
+// 懒加载
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const Cart = () => import('../views/cart/Cart.vue')
+const Profile = () => import('../views/profile/Profile.vue')
+
+
+const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/profile',
+    component: Profile
   }
 ]
 
