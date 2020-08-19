32630169b377569f4f5e4fabc4860d6ca76a35f0
lbing19 <68732146+lbing19@users.noreply.github.com>
2020-08-19 10:27:13 +0800

    首页轮播

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div>
+  <div id='home'>
     <nav-bar class='home-nav'>
-      <div slot='center'>购物街</div>
+      <div slot='center'>嗨购</div>
     </nav-bar>
+    <swiper>
+      <swiper-item v-for='(item, index) in banners' :key='index'>
+        <a :href='item.link'>
+          <img :src='item.image' />
+        </a>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 <script>
 //navbar
 import NavBar from '../../components/common/nevbar/NavBar'
+//请求首页数据getMultidata
+import { getMultidata } from '../../network/home'
+//轮播
+import { Swiper, SwiperItem } from '../../components/common/swiper'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
   components: {
-    NavBar
+    NavBar,
+    Swiper,
+    SwiperItem
+  },
+  created() {
+    getMultidata().then(res => {
+      // console.log(res.data)
+      this.banners = res.data.data.banner.list
+      this.recommends = res.data.data.recommend.list
+    })
   }
 }
 </script>
